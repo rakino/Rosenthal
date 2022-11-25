6e6d098eb9b76d5977c7a0a6c9150d58ef2f6ad0
Hilton Chain <hako@ultrarare.space>
2022-11-25 15:33:39 +0800

    rosenthal: Add pixman-0.42.2.
    * rosenthal/packages/xdisorg.scm (pixman-0.42.2): New variable.

--- rosenthal/packages/xdisorg.scm
+++ rosenthal/packages/xdisorg.scm
@@ -54,3 +54,16 @@
       (inputs
        (modify-inputs (package-inputs base)
          (delete "libx11" "libxcb" "wayland" "wayland-protocols"))))))
+
+(define-public pixman-0.42.2
+  (package
+    (inherit pixman)
+    (name "pixman")
+    (version "0.42.2")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://www.cairographics.org/releases/"
+                                  "pixman-" version ".tar.gz"))
+              (sha256
+               (base32
+                "0pk298iqxqr64vk3z6nhjwr6vjg1971zfrjkqy5r9zd2mppq057a"))))))
