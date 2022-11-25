d67cb6b31279d5e2302214c630006e81f8f683c4
Hilton Chain <hako@ultrarare.space>
2022-11-25 14:27:48 +0800

    rosenthal: Add emacs-company-dev.
    * rosenthal/packages/emacs-xyz.scm: New file.
    (emacs-company-dev): New variable.

---
+++ rosenthal/packages/emacs-xyz.scm
@@ -0,0 +1,28 @@
+;; SPDX-FileCopyrightText: 2022 Hilton Chain <hako@ultrarare.space>
+;;
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(define-module (rosenthal packages emacs-xyz)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix build-system emacs)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (gnu packages emacs-xyz))
+
+(define-public emacs-company-dev
+  (let ((revision "357")
+        (commit "48fea7a905b3bcc6d97609316beced666da89b1f"))
+    (package
+      (inherit emacs-company)
+      (name "emacs-company-dev")
+      (version (git-version "0.9.13" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/company-mode/company-mode")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "17w9irn5aaxadsm5smz5cm1dxy0xb1hh0dnhwqclq3y5llrb21lx")))))))
