be9036707e624cac5fee3cb317737718c7a56888
Hilton Chain <hako@ultrarare.space>
2022-12-09 10:48:14 +0800

    rosenthal: Remove buku-run. [upstreamed]
    * rosenthal/packages/web.scm (buku-run): Delete variable.

--- rosenthal/packages/web.scm
+++ rosenthal/packages/web.scm
@@ -11,48 +11,6 @@
   #:use-module (guix git-download)
   #:use-module (guix packages))
 
-;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=59558
-(define-public buku-run
-  (package
-    (name "buku-run")
-    (version "0.1.1")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/carnager/buku_run")
-                    (commit version)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "1zyjjf3b8g3dnymcrg683rbnc6qrvx8ravfm833n7kjrqky3bczn"))))
-    (build-system gnu-build-system)
-    (arguments
-     (list #:tests? #f                  ;no tests
-           #:make-flags
-           #~(list (string-append "DESTDIR=" #$output)
-                   "PREFIX=")
-           #:phases
-           #~(modify-phases %standard-phases
-               (delete 'configure)
-               (add-after 'unpack 'fixpath
-                 (lambda* (#:key inputs #:allow-other-keys)
-                   (substitute* "buku_run"
-                     ((" \\<(rofi)\\>" all cmd)
-                      (string-append " " (search-input-file inputs "/bin/rofi")))
-                     (("\\<(buku)\\> " all cmd)
-                      (string-append (search-input-file inputs "/bin/buku") " "))
-                     (("\\<(awk|gawk)\\>" cmd)
-                      (search-input-file inputs "/bin/awk"))
-                     (("/etc/buku_run.config" path)
-                      (string-append #$output path))))))))
-    (inputs (list buku rofi))
-    (home-page "https://github.com/carnager/buku_run")
-    (synopsis "Rofi frontend for buku bookmarks manager")
-    (description
-     "This package provides a @code{rofi} frontend for @code{buku} bookmark
-manager.")
-    (license license:gpl3)))
-
 (define-public buku-run-dev
   (let ((revision "23")
         (commit "54fcdd77fc1e8e657b785b7d4ca8dc915e5f336b"))
