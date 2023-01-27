a33c77f33d06d102d364c6d6729543b873ade1bd
Hilton Chain <hako@ultrarare.space>
2023-01-27 16:10:38 +0800

    counter-stop: Add channels.
    * rosenthal/utils/counter-stop.scm (%channel-guix,%channel-nonguix)
    (%channel-rosenthal,%guix-authorized-key-dorphine)
    (%rosenthal-default-channels): New variables.

--- rosenthal/utils/counter-stop.scm
+++ rosenthal/utils/counter-stop.scm
@@ -4,6 +4,7 @@
 
 (define-module (rosenthal utils counter-stop)
   #:use-module (srfi srfi-1)
+  #:use-module (guix channels)
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (gnu packages)
@@ -30,13 +31,18 @@
   #:use-module (gnu system keyboard)
   #:use-module (gnu system shadow)
   #:use-module (rosenthal utils kicksecure)
-  #:export (%guix-authorized-key-nonguix
-            %guix-authorized-key-tobias
+  #:export (%channel-guix
+            %channel-nonguix
+            %channel-rosenthal
+
+            %guix-authorized-key-dorphine
+            %guix-authorized-key-nonguix
 
             normalize-package
 
             %xdg-base-directory-environment-variables
 
+            %rosenthal-default-channels
             %rosenthal-default-kernel-arguments
             %rosenthal-default-keyboard-layout
             %rosenthal-base-initrd-modules
@@ -47,7 +53,45 @@
 ;; Common procedures and variables shared across my home environment and
 ;; operating system definitions.
 
+;; Channels
+(define %channel-guix
+  (first %default-channels))
+
+(define %channel-nonguix
+  (channel
+   (name 'nonguix)
+   (url "https://gitlab.com/nonguix/nonguix")
+   (introduction
+    (make-channel-introduction
+     "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+     (openpgp-fingerprint
+      "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
+
+(define %channel-rosenthal
+  (channel
+   (name 'rosenthal)
+   (url "https://github.com/rakino/rosenthal")
+   (branch "trunk")
+   (introduction
+    (make-channel-introduction
+     "7677db76330121a901604dfbad19077893865f35"
+     (openpgp-fingerprint
+      "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7")))))
+
+(define %rosenthal-default-channels
+  (list %channel-guix
+        %channel-nonguix
+        %channel-rosenthal))
+
 ;; Keys
+;; local
+(define %guix-authorized-key-dorphine
+  (plain-file "dorphine.pub" "
+(public-key
+ (ecc
+  (curve Ed25519)
+  (q #BBE816F9D051E8B715F17DA26B674462DF1967AC77A4130CA3306878314B84AC#)))"))
+
 ;; https://substitutes.nonguix.org/signing-key.pub
 (define %guix-authorized-key-nonguix
   (plain-file "nonguix.pub" "
