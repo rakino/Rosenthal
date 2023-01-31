44c7908c5383dfa2117042f28839d20821949cf8
Hilton Chain <hako@ultrarare.space>
2023-01-31 12:57:31 +0800

    rosenthal: Remove go-github-com-google-gopacket. [upstreamed]
    * rosenthal/packages/golang.scm (go-github-com-google-gopacket): Delete
    variable.

--- rosenthal/packages/golang.scm
+++ rosenthal/packages/golang.scm
@@ -7,25 +7,3 @@
   #:use-module (guix build-system go)
   #:use-module (guix git-download)
   #:use-module (guix packages))
-
-(define-public go-github-com-google-gopacket
-  (package
-    (name "go-github-com-google-gopacket")
-    (version "1.1.19")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/google/gopacket")
-                    (commit (string-append "v" version))))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "048qwm2n0wrpql4qqgd7jyynn3gk069yvqbxnshlayzmbhf87ls4"))))
-    (build-system go-build-system)
-    (arguments
-     '(#:import-path "github.com/google/gopacket"))
-    (home-page "https://github.com/google/gopacket")
-    (synopsis "Packet processing capabilities library")
-    (description
-     "This package provides packet processing capabilities for Go.")
-    (license license:bsd-3)))
