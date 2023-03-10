c4a4ba04a627fdc8d0179965777e14192761311f
Hilton Chain <hako@ultrarare.space>
2023-03-10 14:07:41 +0800

    rosenthal: Remove bubblewrap-0.7.0.
    * rosenthal/packages/virtualization.scm (bubblewrap-0.7.0): Delete variable.
    
    Version 0.8.0 is available in Guix now.
    https://git.savannah.gnu.org/cgit/guix.git/commit/?id=5755c5b33ceb92be4511afd167a0dc17a46a6d3d

--- rosenthal/packages/virtualization.scm
+++ rosenthal/packages/virtualization.scm
@@ -6,20 +6,3 @@
   #:use-module (guix download)
   #:use-module (guix packages)
   #:use-module (gnu packages virtualization))
-
-;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=59114
-(define-public bubblewrap-0.7.0
-  (let ((base bubblewrap))
-    (package
-      (inherit base)
-      (name "bubblewrap")
-      (version "0.7.0")
-      (source (origin
-                (inherit (package-source base))
-                (method url-fetch)
-                (uri (string-append "https://github.com/containers/bubblewrap/"
-                                    "releases/download/v" version
-                                    "/bubblewrap-" version ".tar.xz"))
-                (sha256
-                 (base32
-                  "1p59hawgpf16mc01ybf6dfb2b96pk7h65ls0si9yldyh1c8bfjkn")))))))
