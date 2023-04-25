26653dae61b9f2853682bb29230e2dd81c1efb49
Hilton Chain <hako@ultrarare.space>
2023-04-25 19:11:26 +0800

    rosenthal: Remove libva-2.16.0. [upstreamed]
    * rosenthal/packages/video.scm (libva-2.16.0): Delete variable.

--- rosenthal/packages/video.scm
+++ rosenthal/packages/video.scm
@@ -3,29 +3,13 @@
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
 (define-module (rosenthal packages video)
-  #:use-module (guix download)
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (gnu packages video))
 
-;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=59050
-(define-public libva-2.16.0
-  (package
-    (inherit libva)
-    (name "libva")
-    (version "2.16.0")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append "https://github.com/intel/libva"
-                                  "/releases/download/" version
-                                  "/libva-" version ".tar.bz2"))
-              (sha256
-               (base32
-                "070aj9nw681a4m7f5xb662hhyib0w9q0i0s9v8vplh9cvfhaqpqi"))))))
-
 (define-public libva-nox
-  (let ((base libva-2.16.0))
+  (let ((base libva))
     (package
       (inherit base)
       (name "libva-nox")
