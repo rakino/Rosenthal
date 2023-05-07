35a4bc0849aa6fe9038949012b7784d5d1c28a6f
Hilton Chain <hako@ultrarare.space>
2023-05-07 16:09:38 +0800

    rosenthal: Remove RPi related stuff.
    * rosenthal/bootloader/u-boot.scm: Delete file.
    * rosenthal/packages/bootloaders.scm: Delete file.

--- rosenthal/packages/bootloaders.scm
+++
@@ -1,9 +0,0 @@
-;; SPDX-FileCopyrightText: 2022 Hilton Chain <hako@ultrarare.space>
-;;
-;; SPDX-License-Identifier: GPL-3.0-or-later
-
-(define-module (rosenthal packages bootloaders)
-  #:use-module (gnu packages bootloaders))
-
-(define-public u-boot-rpi-arm64
-  (make-u-boot-package "rpi_arm64" "aarch64-linux-gnu"))
