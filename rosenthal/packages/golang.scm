92ba09a17357ae7585a73763ef04aef2fd478402
Hilton Chain <hako@ultrarare.space>
2023-05-29 14:17:38 +0800

    rosenthal: Remove empty modules.
    * rosenthal/packages/golang.scm: ...
    * rosenthal/packages/python-xyz.scm: ...
    * rosenthal/packages/virtualization.scm: Delete files.

--- rosenthal/packages/golang.scm
+++
@@ -1,9 +0,0 @@
-;; SPDX-FileCopyrightText: 2022 Hilton Chain <hako@ultrarare.space>
-;;
-;; SPDX-License-Identifier: GPL-3.0-or-later
-
-(define-module (rosenthal packages golang)
-  #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix build-system go)
-  #:use-module (guix git-download)
-  #:use-module (guix packages))
--- rosenthal/packages/virtualization.scm
+++
@@ -1,8 +0,0 @@
-;; SPDX-FileCopyrightText: 2022 Hilton Chain <hako@ultrarare.space>
-;;
-;; SPDX-License-Identifier: GPL-3.0-or-later
-
-(define-module (rosenthal packages virtualization)
-  #:use-module (guix download)
-  #:use-module (guix packages)
-  #:use-module (gnu packages virtualization))
