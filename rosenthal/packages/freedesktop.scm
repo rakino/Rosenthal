8aa643c684f5bf3fff8730ea69f58e8c1ca5a1e2
Hilton Chain <hako@ultrarare.space>
2023-06-02 11:32:18 +0800

    rosenthal: Add libinput-minimal-1.23.0.
    * rosenthal/packages/freedesktop.scm (libinput-minimal-1.23.0): New variable.

--- rosenthal/packages/freedesktop.scm
+++ rosenthal/packages/freedesktop.scm
@@ -5,9 +5,31 @@
 (define-module (rosenthal packages freedesktop)
   #:use-module (guix download)
   #:use-module (guix gexp)
+  #:use-module (guix git-download)
   #:use-module (guix packages)
   #:use-module (guix utils)
-  #:use-module (gnu packages freedesktop))
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages python))
+
+;; https://issues.guix.gnu.org/63847
+(define-public libinput-minimal-1.23.0
+  (package
+    (inherit libinput-minimal)
+    (name "libinput-minimal")
+    (version "1.23.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://gitlab.freedesktop.org/libinput/libinput.git")
+                    (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0wnqfnxxvf9jclh64hrb0scn3s1dmwdkmqf7hp0cfmjz5n5nnv7d"))))
+    (native-inputs
+     (modify-inputs (package-native-inputs libinput)
+       (append python-minimal-wrapper python-pytest)))))
 
 (define-public wayland-protocols-1.31
   (let ((base wayland-protocols))
