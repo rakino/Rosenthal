8470280b87e622a3c2b5bacb75697e6b036e50ef
Ryan Schanzenbacher <ryan@rschanz.org>
2023-06-11 14:07:21 -0400

    added xdg-desktop-portal-hyprland and associated hyprland-share-picker helper utility

--- rosenthal/packages/freedesktop.scm
+++ rosenthal/packages/freedesktop.scm
@@ -46,3 +46,46 @@
                 (sha256
                  (base32
                   "0f72359fzvh6jzri4fd79m34rwm2r55p2ryq4306wrw7xliafzx0")))))))
+
+(define-public xdg-desktop-portal-hyprland
+  (package
+    (name "xdg-desktop-portal-hyprland")
+    (version "0.4.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                     (url "https://github.com/hyprwm/xdg-desktop-portal-hyprland")
+                     (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+                (base32
+                  "1dcglmx02j73qbmw9qsacamn8byakyzwknpqjnhsyphixb4crrdg"))))
+    (build-system meson-build-system)
+    (arguments
+      (list
+        #:configure-flags #~(list "-Dsystemd=disabled")))
+    (native-inputs (list cmake pkg-config wayland hyprland-protocols))
+    (inputs (list elogind hyprland-protocols pipewire wayland-protocols wayland libinih `(,util-linux "lib")))
+    (home-page "https://github.com/hyprwm/xdg-desktop-portal-hyprland")
+    (synopsis "@code{xdg-desktop-portal} backend for Hyprland")
+    (description
+      "This package provides @code{xdg-desktop-portal-hyprland}. This project extends @code{xdg-desktop-portal-wlr} for Hyprland, adding support for @code{xdg-desktop-portal} screenshot and casting interfaces while adding a few extra portals specific to Hyprland: mostly for window sharing. Requires @code{hyprland-share-picker}.")
+    (license license:expat)))
+
+(define-public hyprland-share-picker
+  (package
+    (inherit xdg-desktop-portal-hyprland)
+    (name "hyprland-share-picker")
+    (build-system qt-build-system)
+    (inputs (modify-inputs (package-inputs xdg-desktop-portal-hyprland)
+                           (append qtwayland-5)))
+    (native-inputs (modify-inputs (package-native-inputs xdg-desktop-portal-hyprland)
+                                  (append qtwayland-5)))
+    (arguments
+      (list
+        #:tests? #f
+        #:phases
+        #~(modify-phases %standard-phases
+            (add-after 'unpack 'chdir
+              (lambda _ (chdir "hyprland-share-picker"))))))
+    (synopsis "Helper program for @code{xdg-desktop-portal-hyprland}.")))
