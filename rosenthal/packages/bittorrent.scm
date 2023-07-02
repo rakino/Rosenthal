ed1058c688a2655521beeba3e6295db2c0c1f274
Hilton Chain <hako@ultrarare.space>
2023-07-02 16:47:01 +0800

    rosenthal: Remove qbittorrent-enhanced. [upstreamed]
    * rosenthal/packages/bittorrent.scm (qbittorrent-enhanced)
    (qbittorrent-enhanced-edition): Delete variables.

--- rosenthal/packages/bittorrent.scm
+++ rosenthal/packages/bittorrent.scm
@@ -10,34 +10,3 @@
   #:use-module (guix utils)
   #:use-module (gnu packages bittorrent)
   #:use-module (gnu packages qt))
-
-(define-public qbittorrent-enhanced
-  (let ((base qbittorrent))
-    (package
-      (inherit base)
-      (name "qbittorrent-enhanced")
-      (version "4.5.4.10")
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/c0re100/qBittorrent-Enhanced-Edition")
-                      (commit (string-append "release-" version))))
-                (file-name (git-file-name name version))
-                (sha256
-                 (base32
-                  "0qwk69mgcyh7fij4nsi4ndd17aa61p2c6cxn9l402w4cf1dy6hfs"))))
-      (home-page "https://github.com/c0re100/qBittorrent-Enhanced-Edition")
-      (description
-       "qBittorrent Enhanced is a bittorrent client based on qBittorrent with
-the following features:
-
-@itemize
-@item Auto Ban Xunlei, QQ, Baidu, Xfplay, DLBT and Offline downloader
-@item Auto Ban Unknown Peer from China Option (Default: OFF)
-@item Auto Update Public Trackers List (Default: OFF)
-@item Auto Ban BitTorrent Media Player Peer Option (Default: OFF)
-@item Peer whitelist/blacklist
-@end itemize\n"))))
-
-(define-public qbittorrent-enhanced-edition
-  (deprecated-package "qbittorrent-enhanced-edition" qbittorrent-enhanced))
