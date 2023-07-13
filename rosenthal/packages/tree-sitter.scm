091c21ec8b8dea5671bc081d679e19f7c808a0d0
Hilton Chain <hako@ultrarare.space>
2023-07-13 21:25:44 +0800

    rosenthal: Add tree-sitter-dockerfile.
    * rosenthal/packages/tree-sitter.scm (tree-sitter-dockerfile): New variable.

--- rosenthal/packages/tree-sitter.scm
+++ rosenthal/packages/tree-sitter.scm
@@ -11,3 +11,11 @@
    "0i8vsvkkas1fnn35na68gq5xq2y3xvhv2bk9q6x8c1bzsqm6rcsa"
    "0.4.0"
    #:repository-url "https://github.com/uyha/tree-sitter-cmake"))
+
+;; <https://issues.guix.gnu.org/64431>
+(define-public tree-sitter-dockerfile
+  (tree-sitter-grammar
+   "dockerfile" "Dockerfile"
+   "0kf4c4xs5naj8lpcmr3pbdvwj526wl9p6zphxxpimbll7qv6qfnd"
+   "0.1.2"
+   #:repository-url "https://github.com/camdencheek/tree-sitter-dockerfile"))
