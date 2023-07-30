c8c4a6f991008d81dfbbf3f1edd957c22b6b1e77
Hilton Chain <hako@ultrarare.space>
2023-07-30 22:24:29 +0800

    rosenthal: Remove wayland-protocols-1.31.
    wayland-protocols in Guix has been updated to 1.32, this package is not needed.
    
    * rosenthal/packages/freedesktop.scm (wayland-protocols-1.31): Delete variable.
    * rosenthal/packages/wm.scm (wlroots-for-hyprland)[propagated-inputs]: Don't
    replace wayland-protocols.

--- rosenthal/packages/freedesktop.scm
+++ rosenthal/packages/freedesktop.scm
@@ -30,19 +30,3 @@
     (native-inputs
      (modify-inputs (package-native-inputs libinput)
        (append python-minimal-wrapper python-pytest)))))
-
-(define-public wayland-protocols-1.31
-  (let ((base wayland-protocols))
-    (package
-      (inherit base)
-      (name "wayland-protocols")
-      (version "1.31")
-      (source (origin
-                (method url-fetch)
-                (uri (string-append
-                      "https://gitlab.freedesktop.org/wayland/wayland-protocols"
-                      "/-/releases/" version "/downloads/"
-                      "wayland-protocols-" version ".tar.xz"))
-                (sha256
-                 (base32
-                  "0f72359fzvh6jzri4fd79m34rwm2r55p2ryq4306wrw7xliafzx0")))))))
