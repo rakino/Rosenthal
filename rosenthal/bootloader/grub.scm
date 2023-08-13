939ec4a7c4cfb5709b9e55f5968ac758828883d4
Hilton Chain <hako@ultrarare.space>
2023-08-14 01:23:11 +0800

    bootloader: Add grub-efi-luks2-bootloader.
    * rosenthal/bootloader/grub.scm: New file.
    (grub-efi-luks2-bootloader): New variable.

---
+++ rosenthal/bootloader/grub.scm
@@ -0,0 +1,16 @@
+;; SPDX-FileCopyrightText: 2023 Hilton Chain <hako@ultrarare.space>
+;;
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(define-module (rosenthal bootloader grub)
+  #:use-module (gnu bootloader)
+  #:use-module (gnu bootloader grub)
+  #:use-module (gnu packages bootloaders)
+  #:use-module (rosenthal packages bootloaders)
+  #:export (grub-efi-luks2-bootloader))
+
+(define grub-efi-luks2-bootloader
+  (bootloader
+   (inherit grub-efi-bootloader)
+   (name 'grub-efi-luks2-bootloader)
+   (package grub-efi-luks2)))
