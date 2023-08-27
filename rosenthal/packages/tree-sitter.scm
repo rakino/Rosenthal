c77c36714978b4edc14570e142e4b94b0b9f06c4
Hilton Chain <hako@ultrarare.space>
2023-08-27 12:46:54 +0800

    rosenthal: Remove tree-sitter-cmake. [upstreamed]
    * rosenthal/packages/tree-sitter.scm (tree-sitter-cmake): Delete variable.
    * README.org (Packages): Remove it.

--- rosenthal/packages/tree-sitter.scm
+++ rosenthal/packages/tree-sitter.scm
@@ -4,14 +4,6 @@
 (define tree-sitter-grammar
   (@@ (gnu packages tree-sitter) tree-sitter-grammar))
 
-;; <https://issues.guix.gnu.org/64430>
-(define-public tree-sitter-cmake
-  (tree-sitter-grammar
-   "cmake" "CMake"
-   "0i8vsvkkas1fnn35na68gq5xq2y3xvhv2bk9q6x8c1bzsqm6rcsa"
-   "0.4.0"
-   #:repository-url "https://github.com/uyha/tree-sitter-cmake"))
-
 (define-public tree-sitter-yaml
   (let ((base
          (tree-sitter-grammar
