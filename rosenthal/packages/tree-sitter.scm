f84002986f8e0080e13b8ea61070b3ce392b5fc8
Hilton Chain <hako@ultrarare.space>
2023-11-23 20:04:19 +0800

    rosenthal: tree-sitter-yaml: Remove bundled binary.
    * rosenthal/packages/tree-sitter.scm (tree-sitter-yaml)
    [#:get-cleanup-snippet]: Remove docs directory.

--- rosenthal/packages/tree-sitter.scm
+++ rosenthal/packages/tree-sitter.scm
@@ -1,4 +1,5 @@
 (define-module (rosenthal packages tree-sitter)
+  #:use-module (guix gexp)
   #:use-module (guix packages))
 
 (define tree-sitter-grammar
@@ -10,7 +11,22 @@
           "yaml" "YAML"
           "1bimf5fq85wn8dwlk665w15n2bj37fma5rsfxrph3i9yb0lvzi3q"
           "0.5.0"
-          #:repository-url "https://github.com/ikatyang/tree-sitter-yaml")))
+          #:repository-url "https://github.com/ikatyang/tree-sitter-yaml"
+          #:get-cleanup-snippet
+          (lambda (grammar-directories)
+            #~(begin
+                (use-modules (guix build utils))
+                (delete-file-recursively "docs")
+                (delete-file "binding.gyp")
+                (delete-file-recursively "bindings")
+                (for-each
+                 (lambda (lang)
+                   (with-directory-excursion lang
+                     (delete-file "src/grammar.json")
+                     (delete-file "src/node-types.json")
+                     (delete-file "src/parser.c")
+                     (delete-file-recursively "src/tree_sitter")))
+                 '#$grammar-directories))))))
     (package
       (inherit base)
       (arguments
