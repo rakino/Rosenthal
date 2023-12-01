ee4fab769b9dffe4680848a2e0a5c3cb4a1e9000
Hilton Chain <hako@ultrarare.space>
2023-12-01 13:29:54 +0800

    rosenthal: Remove flashrom-dev.
    * rosenthal/packages/flashing-tools.scm (flashrom-dev): Delete variable.

--- rosenthal/packages/flashing-tools.scm
+++ rosenthal/packages/flashing-tools.scm
@@ -8,27 +8,3 @@
   #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (gnu packages flashing-tools))
-
-(define-public flashrom-dev
-  (let ((base flashrom)
-        (revision "1165")
-        (commit "7ffa626d1278a167c430c55891bbb6f979a5ab92"))
-    (package
-      (inherit base)
-      (name "flashrom-dev")
-      (version (git-version "1.2" revision commit))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/flashrom/flashrom")
-                      (commit commit)))
-                (file-name (git-file-name name version ))
-                (sha256
-                 (base32
-                  "015s56ygl4bp4awvxnx589xqc9mlxmyr0r8dk1rgy8zp2wbwhknh"))))
-      (arguments
-       (substitute-keyword-arguments (package-arguments base)
-         ((#:make-flags _)
-          #~(list (string-append "CC=" #$(cc-for-target))
-                  (string-append "PREFIX=" #$output)
-                  "CONFIG_ENABLE_LIBUSB0_PROGRAMMERS=no")))))))
