3d8dbdb97b6ed6d4703c773cddf683a803521a2e
Hilton Chain <hako@ultrarare.space>
2024-01-25 20:36:05 +0800

    rosenthal: Add pam-uaccess.
    * rosenthal/packages/admin.scm (pam-uaccess): New variable.

--- rosenthal/packages/admin.scm
+++ rosenthal/packages/admin.scm
@@ -3,10 +3,16 @@
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
 (define-module (rosenthal packages admin)
+  #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix utils)
-  #:use-module (gnu packages admin))
+  #:use-module (guix git-download)
+  #:use-module (guix build-system meson)
+  #:use-module (gnu packages acl)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages pkg-config))
 
 (define-public libseat-sans-logind
   (let ((base libseat))
@@ -31,3 +37,30 @@
           #~(append #$configure-flags
                     (list "-Dlibseat-logind=disabled")))))
       (propagated-inputs '()))))
+
+(define-public pam-uaccess
+  (let ((commit "54fbf043c63cc500b4850b0b4a12ea14078f2b53")
+        (revision "0"))
+    (package
+      (name "pam-uaccess")
+      (version (git-version "0.0.0" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://git.sr.ht/~kennylevinsen/pam_uaccess")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "08068cw4nvcanym8b5dyccnnb3qc3f09pbvi6fcfiz227yx73npc"))))
+      (build-system meson-build-system)
+      (native-inputs (list pkg-config))
+      (inputs (list acl eudev linux-pam))
+      (home-page "https://git.sr.ht/~kennylevinsen/pam_uaccess")
+      (synopsis
+       "PAM module that grants access to devices tagged @code{uaccess} in udev")
+      (description
+       "@code{pam_uaccess} is a PAM module that grants access to devices tagged
+@code{uaccess} in udev for the duration of the users' session, replacing
+elogind's uaccess feature.")
+      (license license:expat))))
