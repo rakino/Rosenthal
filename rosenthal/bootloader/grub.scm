06eb07b022a3238404f684935dc0135f643095ca
Hilton Chain <hako@ultrarare.space>
2024-03-25 22:01:03 +0800

    bootloader: grub: Add grub-efi-luks2-removable-bootloader.
    * rosenthal/bootloader/grub.scm (grub-efi-luks2-removable-bootloader): New
    variable.

--- rosenthal/bootloader/grub.scm
+++ rosenthal/bootloader/grub.scm
@@ -6,7 +6,8 @@
   #:use-module (gnu bootloader)
   #:use-module (gnu bootloader grub)
   #:use-module (rosenthal packages bootloaders)
-  #:export (grub-efi-luks2-bootloader))
+  #:export (grub-efi-luks2-bootloader
+            grub-efi-luks2-removable-bootloader))
 
 (define grub-efi-luks2-bootloader
   (bootloader
@@ -14,4 +15,9 @@
    ;; NOTE: Don't change the name.  Generation switching code only knows
    ;; bootloaders defined in (gnu bootloader grub).
    (name 'grub-efi)
+   (package grub-efi-luks2)))
+
+(define grub-efi-luks2-removable-bootloader
+  (bootloader
+   (inherit grub-efi-removable-bootloader)
    (package grub-efi-luks2)))
