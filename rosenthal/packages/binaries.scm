4b9d1af9a74c4bd4816b69331bad43f90ad56948
Hilton Chain <hako@ultrarare.space>
2025-03-01 13:54:04 +0800

    rosenthal: komga-bin: Update to 1.20.0.
    * rosenthal/packages/binaries.scm (komga-bin): Update to 1.20.0.

--- rosenthal/packages/binaries.scm
+++ rosenthal/packages/binaries.scm
@@ -230,7 +230,7 @@ designed for flexibility.")
 (define-public komga-bin
   (package
    (name "komga-bin")
-   (version "1.19.1")
+   (version "1.20.0")
    (source (origin
              (method url-fetch)
              (uri (string-append
@@ -238,7 +238,7 @@ designed for flexibility.")
                    "/komga-" version ".jar"))
              (sha256
               (base32
-               "167nihqx7lp2hiq49smjbxw9d2pz8bdkqvl0j82s7d4s79h5qmy8"))))
+               "1dyxavr2jn85appysj8j88i2x3fv46l5s25x4gb47hr4n68f230n"))))
    (build-system copy-build-system)
    (arguments
     (list #:install-plan
