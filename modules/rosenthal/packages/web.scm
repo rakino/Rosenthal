ebc3fabd2d559fcdb7fac095afad6dcbb6f864c4
Hilton Chain <hako@ultrarare.space>
2025-06-06 16:48:48 +0800

    rosenthal: ai-robots-txt: Update to 1.34.
    * modules/rosenthal/packages/web.scm (ai-robots-txt): Update to 1.34.

--- modules/rosenthal/packages/web.scm
+++ modules/rosenthal/packages/web.scm
@@ -21,7 +21,7 @@
 (define-public ai-robots-txt
   (package
     (name "ai-robots-txt")
-    (version "1.31")
+    (version "1.34")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -30,7 +30,7 @@
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "1bvq24w8pq56knhdacjkq93v6l719jcj3jf4fsknlmp9m6izm3zj"))
+                "1fccxli6iszf21p8yql1p4969b715qi1z3d99nqkar4mmgpaakgn"))
               (modules '((guix build utils)))
               (snippet '(delete-file-recursively "code"))))
     (build-system copy-build-system)
