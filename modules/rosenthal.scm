b084fdae98d94cf2a1f65b36172f533e96312776
Hilton Chain <hako@ultrarare.space>
2025-06-22 12:02:33 +0800

    Add (rosenthal) module.
    * modules/rosenthal.scm: New file.

---
+++ modules/rosenthal.scm
@@ -0,0 +1,32 @@
+;;; SPDX-FileCopyrightText: 2025 Hilton Chain <hako@ultrarare.space>
+;;;
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(define-module (rosenthal))
+
+;; Re-export commonly-used modules.
+
+(eval-when (eval load compile)
+  (begin
+    (define %public-modules
+      '((gnu)
+        (guix utils)
+        (rosenthal utils file)
+        (rosenthal utils packages)
+        (rosenthal utils transformations)
+
+        (gnu services desktop)
+        (gnu services guix)
+        (gnu services shepherd)
+        (rosenthal services base)
+        (rosenthal services desktop)
+
+        (gnu home)
+        (gnu home services)
+        (gnu home services desktop)
+        (gnu home services shepherd)))
+
+    (for-each (let ((i (module-public-interface (current-module))))
+                (lambda (m)
+                  (module-use! i (resolve-interface m))))
+              %public-modules)))
