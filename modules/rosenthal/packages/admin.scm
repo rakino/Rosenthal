127d1be3670255a75e7b407698209aee98d5436e
Hilton Chain <hako@ultrarare.space>
2025-08-21 19:40:07 +0800

    rosenthal: Remove tuigreet. [upstreamed]
    * modules/rosenthal/packages/admin.scm (tuigreet): Delete variable.

--- modules/rosenthal/packages/admin.scm
+++ modules/rosenthal/packages/admin.scm
@@ -116,26 +116,3 @@ exists and is only writable by root.")
                     (list "-Dlibseat-logind=disabled")))))
       (propagated-inputs '())
       (properties '((disable-updater? . #t))))))
-
-(define-public tuigreet
-  (package
-    (name "tuigreet")
-    (version "0.9.1")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                     (url "https://github.com/apognu/tuigreet")
-                     (commit version)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "15h2b97clllbhlw5jc4lwkmir18njnyk56zghafsas84m6jjsikv"))))
-    (build-system cargo-build-system)
-    (arguments (list #:install-source? #f))
-    (inputs (rosenthal-cargo-inputs 'tuigreet))
-    (home-page "https://github.com/apognu/tuigreet")
-    (synopsis "Graphical console greeter for @code{greetd}")
-    (description
-     "This package provides a graphical console greeter for @code{greetd}.  It
-doesn't need a Wayland compositor to be used.")
-    (license license:gpl3+)))
