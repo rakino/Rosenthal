9c49db368fb9953f2be97e37220d32922260bf1d
Hilton Chain <hako@ultrarare.space>
2025-08-21 19:40:22 +0800

    rosenthal: Remove rust-smithay. [upstreamed]
    * modules/rosenthal/packages/wm.scm (rust-smithay): Delete variable.

--- modules/rosenthal/packages/wm.scm
+++ modules/rosenthal/packages/wm.scm
@@ -20,59 +20,6 @@
   #:use-module (gnu packages wm)
   #:use-module (gnu packages xdisorg))
 
-(define-public rust-smithay
-  (let ((commit "0cd3345c59f7cb139521f267956a1a4e33248393")
-        (revision "0"))
-    (package
-      (name "rust-smithay")
-      (version (git-version "0.4.0" revision commit))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/Smithay/smithay")
-                      (commit commit)))
-                (file-name (git-file-name name version))
-                (sha256
-                 (base32
-                  "191h87bpzg0l1ihfb4hmx00b86pfb5mwwc6s8i49al0vigc14l37"))))
-      (build-system cargo-build-system)
-      (arguments
-       (list #:skip-build? #t
-             #:phases
-             #~(modify-phases %standard-phases
-                 (replace 'package
-                   (lambda* (#:key cargo-package-flags vendor-dir #:allow-other-keys)
-                     (begin
-                       ;;error: invalid inclusion of reserved file name Cargo.toml.orig in package source
-                       (when (file-exists? "Cargo.toml.orig")
-                         (delete-file "Cargo.toml.orig"))
-
-                       (for-each
-                        (lambda (pkg)
-                          (apply invoke "cargo" "package" "--offline" "--package" pkg
-                                 cargo-package-flags)
-                          (for-each
-                           (lambda (crate)
-                             (invoke "tar" "xzf" crate "-C" vendor-dir))
-                           (find-files "target/package" "\\.crate$"))
-                          ((assoc-ref %standard-phases 'patch-cargo-checksums)))
-                        '("smithay" "smithay-drm-extras"))))))))
-      (inputs (rosenthal-cargo-inputs 'rust-smithay))
-      (home-page "https://github.com/Smithay/smithay")
-      (synopsis "Smithy for Rust Wayland compositors")
-      (description
-       "Smithay aims to provide building blocks to create wayland compositors in
-Rust.  While not being a full-blown compositor, it'll provide objects and
-interfaces implementing common functionalities that pretty much any compositor
-will need, in a generic fashion.
-
-It supports the @code{wayland}, @code{wayland-protocols}, and some external
-extensions, such as @code{wlr-protocols} and @code{plasma-wayland-protocols}.")
-      (license license:expat)
-      (properties
-       '((hidden? . #t)
-         (disable-updater? . #t))))))
-
 (define-public niri
   (package
    (name "niri")
