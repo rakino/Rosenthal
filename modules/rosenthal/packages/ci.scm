3d5946a60427c94e04f599c445a53f4a0c22a452
Hilton Chain <hako@ultrarare.space>
2025-09-06 21:45:56 +0800

    rosenthal: Remove cuirass/hako.
    * modules/rosenthal/packages/ci.scm (cuirass/hako): Delete variable.

--- modules/rosenthal/packages/ci.scm
+++ modules/rosenthal/packages/ci.scm
@@ -8,22 +8,3 @@
   #:use-module (guix packages)
   #:use-module (guix git-download)
   #:use-module (gnu packages ci))
-
-(define-public cuirass/hako
-  (let ((commit "ba77a7cc44894e959665941121c133b9b3304768")
-        (revision "3"))
-    (package
-      (inherit cuirass)
-      (name "cuirass-hako")
-      (version (git-version "1.2.0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://codeberg.org/guix/cuirass.git")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32
-           "17s1jah2k276klqxcc7yvcl46n4by2s1q4ipi9icssabfpv4s6yp"))))
-      (properties '((disable-updater? . #t))))))
