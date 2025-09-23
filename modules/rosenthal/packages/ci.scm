f1fa3f49ba5b48bb51d2501a7cffd6f8c3b363a5
Hilton Chain <hako@ultrarare.space>
2025-09-23 23:26:40 +0800

    rosenthal: Add cuirass/dolly.
    * modules/rosenthal/packages/ci.scm (cuirass/dolly): New variable.

--- modules/rosenthal/packages/ci.scm
+++ modules/rosenthal/packages/ci.scm
@@ -8,3 +8,20 @@
   #:use-module (guix packages)
   #:use-module (guix git-download)
   #:use-module (gnu packages ci))
+
+(define-public cuirass/dolly
+  (let ((commit "6ea7d8d94010a00548968fcc3dd493af383c4000")
+        (revision "1"))
+    (package
+      (inherit cuirass)
+      (name "cuirass-dolly")
+      (version (git-version "1.3.1" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                       (url "https://git.guix.gnu.org/cuirass.git")
+                       (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1clzkzclzqfivv7cpwh8r20j8j5nzfnzbf1rrnhcyc1gvlds7blb")))))))
