d90148509cf3fd085c779024b6a0b1ae3b9c0a63
Hilton Chain <hako@ultrarare.space>
2026-02-15 19:32:48 +0800

    rosenthal: %rosenthal-skeletons: Add fcitx5 configuration.
    * modules/rosenthal/examples/dot-config/fcitx5/config: New file.
    * modules/rosenthal/services/desktop.scm (%rosenthal-skeletons): Add it.

--- modules/rosenthal/services/desktop.scm
+++ modules/rosenthal/services/desktop.scm
@@ -580,6 +580,8 @@ gtk-key-theme-name = ~a~%"
 
     (".config/autostart/shepherd.desktop"
      ,(local-file "../examples/dot-config/autostart/shepherd.desktop"))
+    (".config/fcitx5/config"
+     ,(local-file "../examples/dot-config/fcitx5/config"))
     (".config/niri/config.kdl"
      ,(local-file "../examples/dot-config/niri/config.kdl"))
     (".config/xfce4/helpers.rc"
