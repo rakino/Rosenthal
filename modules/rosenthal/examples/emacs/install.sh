ec978c44fa815ec6a8d36a46ec3c8c6900771c07
Hilton Chain <hako@ultrarare.space>
2026-02-16 14:08:09 +0800

    examples: emacs: Fix manifest.
    Guile treats .scm extensions as modules.
    
    * modules/rosenthal/examples/emacs/manifest.scm: Rename to...
    * modules/rosenthal/examples/emacs/manifest.tmpl: ...this new file.
    * modules/rosenthal/examples/emacs/install.sh: Adjust accordingly.

--- modules/rosenthal/examples/emacs/install.sh
+++ modules/rosenthal/examples/emacs/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 dir="$(dirname "$(realpath "$0")")"
 [[ ! -e ~/.config/emacs/init.el ]] && cp --recursive "$dir/." ~/.config/emacs
-guix package --install --manifest="$dir/manifest.scm"
+guix package --install --manifest="$dir/manifest.tmpl"
