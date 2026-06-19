b8f6b048c97ef3c79c115f1933aac8b4c4a02bde
Hilton Chain <hako@ultrarare.space>
2026-06-19 19:53:42 +0800

    rosenthal: mirror-substitutes: Update to 0.0.1.
    * modules/rosenthal/packages/package-management.scm (mirror-substitutes): Update
    to 0.0.1.

--- modules/rosenthal/packages/package-management.scm
+++ modules/rosenthal/packages/package-management.scm
@@ -31,63 +31,59 @@
     (properties '((disable-updater? . #t)))))
 
 (define-public mirror-substitutes
-  (let ((commit "cd6005b2b05ed2cb38a64e30886d387e679b361a")
-        (revision "2"))
-    (package
-      (name "mirror-substitutes")
-      (version (git-version "0.0.0" revision commit))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                       (url "https://codeberg.org/hako/mirror-substitutes.git")
-                       (commit commit)))
-                (file-name (git-file-name name version))
-                (sha256
-                 (base32
-                  "0xrngcvi6xgbz0z99wkg7sl3yw3y92v3hijmbsrbx13bgzbak0yy"))))
-      (build-system guile-build-system)
-      (arguments
-       (list
-        #:source-directory "modules"
-        #:phases
-        #~(modify-phases %standard-phases
-            (add-after 'build 'install
-              (lambda* (#:key inputs #:allow-other-keys)
-                (define-values (module-dir object-dir)
-                  (target-guile-scm+go #$output))
-                (define bindir
-                  (in-vicinity #$output "bin"))
-                (with-directory-excursion "scripts"
-                  (for-each
-                   (lambda (script)
-                     (substitute* script
-                       (("/usr/(bin/env) -S guix" _ env)
-                        (format #f "~a -S ~a"
-                                (search-input-file inputs env)
-                                (if (string-suffix? "query-substitutes" script)
-                                    "guix"
-                                    (search-input-file inputs "bin/guix")))))
-                     (install-file script bindir)
-                     (with-directory-excursion bindir
-                       (wrap-program script
-                         `("GUILE_LOAD_PATH" prefix
-                           (,module-dir))
-                         `("GUILE_LOAD_COMPILED_PATH" prefix
-                           (,object-dir)))))
-                   (find-files "."))))))))
-      (native-inputs
-       (list bash-minimal
-             guile-3.0-latest
-             guix))
-      (inputs
-       (list bash-minimal
-             coreutils-minimal
-             guix))
-      (home-page "https://codeberg.org/hako/mirror-substitutes")
-      (synopsis
-       "Mirror Guix substitutes without relying on remote server implementation")
-      (description
-       "This package provides commands to mirror substitutes for Guix and Nix
-without relying on the implementation of remote server.")
-      (license license:gpl3+)
-      (properties '((disable-updater? . #t))))))
+  (package
+    (name "mirror-substitutes")
+    (version "0.0.1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                     (url "https://codeberg.org/hako/mirror-substitutes.git")
+                     (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0jqlipzwxml91rypdjipfzvplq605hjiy6d3avkfpwasal2ajjh1"))))
+    (build-system guile-build-system)
+    (arguments
+     (list
+      #:source-directory "modules"
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'build 'install
+            (lambda* (#:key inputs #:allow-other-keys)
+              (define-values (module-dir object-dir)
+                (target-guile-scm+go #$output))
+              (define bindir
+                (in-vicinity #$output "bin"))
+              (with-directory-excursion "scripts"
+                (for-each
+                 (lambda (script)
+                   (substitute* script
+                     (("/usr/(bin/env) -S guix" _ env)
+                      (format #f "~a -S ~a"
+                              (search-input-file inputs env)
+                              (if (string-suffix? "query-substitutes" script)
+                                  "guix"
+                                  (search-input-file inputs "bin/guix")))))
+                   (install-file script bindir)
+                   (with-directory-excursion bindir
+                     (wrap-program script
+                       `("GUILE_LOAD_PATH" prefix
+                         (,module-dir))
+                       `("GUILE_LOAD_COMPILED_PATH" prefix
+                         (,object-dir)))))
+                 (find-files "."))))))))
+    (native-inputs
+     (list bash-minimal
+           guile-3.0-latest
+           guix))
+    (inputs
+     (list bash-minimal
+           coreutils-minimal
+           guix))
+    (home-page "https://codeberg.org/hako/mirror-substitutes")
+    (synopsis "Mirror Guix substitutes")
+    (description
+     "This package provides a collection of commands to help mirror Guix / Nix
+substitutes (binary cache) from any upstream server.")
+    (license license:gpl3+)))
