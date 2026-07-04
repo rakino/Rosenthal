6cf57b252d4cfdbe23d5be705bfbae259b3b3400
Hilton Chain <hako@ultrarare.space>
2026-07-04 22:21:48 +0800

    rosenthal: noctalia: Update to 5.0.0-beta1.
    * modules/rosenthal/packages/wm.scm (rosenthal): Update to 5.0.0-beta1.
    [arguments]: Enable tests.
    [inputs]: Add wireplumber.

--- modules/rosenthal/packages/wm.scm
+++ modules/rosenthal/packages/wm.scm
@@ -46,71 +46,63 @@
 
 ;; TODO: Unbundle dependencies under the third_party directory.
 (define-public noctalia
-  (let ((commit "2070b4b70cd78931cbf87439acc2cc6dbdff4174")
-        (revision "1"))
-    (package
-      (name "noctalia")
-      (version (git-version "5.0.0" revision commit))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                       (url "https://github.com/noctalia-dev/noctalia-shell")
-                       (commit commit)))
-                (file-name (git-file-name name version))
-                (sha256
-                 (base32
-                  "18j09sck1pm8p4npvrpykjzvb4613j4k795vd1j637pq37vw759a"))))
-      (build-system meson-build-system)
-      (arguments
-       (list #:build-type "release"
-             ;; FIXME: process_test fails with:
-             ;; --8<---------------cut here---------------start------------->8---
-             ;; stderr:
-             ;; process_test: completion-only async command exit code was wrong
-             ;; process_test: completion-only async command stdout was wrong
-             ;; --8<---------------cut here---------------end--------------->8---
-             #:tests? #f
-             #:phases
-             #~(modify-phases %standard-phases
-                 (add-after 'unpack 'prepare-for-build
-                   (lambda _
-                     ;; For reproducibility.
-                     (substitute* "meson.build"
-                       (("'-march=native', '-mtune=native',") ""))
-                     ;; /bin/sh doesn't exist in the build environment.
-                     (substitute* "tests/process_test.cpp"
-                       (("/bin/(sh)" _ cmd)
-                        (which cmd))))))))
-      (native-inputs
-       (list pkg-config))
-      (inputs
-       (list cairo
-             curl
-             fontconfig
-             freetype
-             glib
-             gmp
-             mpfr
-             harfbuzz
-             jemalloc
-             (librsvg-for-system)
-             libqalculate
-             libwebp
-             libxkbcommon
-             libxml2
-             linux-pam
-             mesa
-             pango
-             pipewire
-             polkit
-             sdbus-c++
-             wayland
-             wayland-protocols-1.48))
-      (home-page "https://noctalia.dev/")
-      (synopsis "Wayland shell and bar")
-      (description
-       "Noctalia is a lightweight Wayland shell and bar built directly on
+  (package
+    (name "noctalia")
+    (version "5.0.0-beta1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                     (url "https://github.com/noctalia-dev/noctalia-shell")
+                     (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "194fhlxn79d8hg0qgczk51jrbnifnvhgikz6npqirzb0kfifxyz9"))))
+    (build-system meson-build-system)
+    (arguments
+     (list #:build-type "release"
+           #:phases
+           #~(modify-phases %standard-phases
+               (add-after 'unpack 'prepare-for-build
+                 (lambda _
+                   ;; For reproducibility.
+                   (substitute* "meson.build"
+                     (("'-march=native', '-mtune=native',") ""))
+                   ;; /bin/sh doesn't exist in the build environment.
+                   (substitute* "tests/process_test.cpp"
+                     (("/bin/(sh)" _ cmd)
+                      (which cmd))))))))
+    (native-inputs
+     (list pkg-config))
+    (inputs
+     (list cairo
+           curl
+           fontconfig
+           freetype
+           glib
+           gmp
+           mpfr
+           harfbuzz
+           jemalloc
+           (librsvg-for-system)
+           libqalculate
+           libwebp
+           libxkbcommon
+           libxml2
+           linux-pam
+           mesa
+           pango
+           pipewire
+           polkit
+           sdbus-c++
+           wayland
+           wayland-protocols-1.48
+           wireplumber))
+    (home-page "https://noctalia.dev/")
+    (synopsis "Wayland shell and bar")
+    (description
+     "Noctalia is a lightweight Wayland shell and bar built directly on
 Wayland and OpenGL ES, with no Qt or GTK dependency.")
-      (license license:expat))))
+    (license license:expat)))
 
 (define-deprecated-package noctalia-shell noctalia)
